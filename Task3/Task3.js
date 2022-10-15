bd2e77914a5ad40ff584c467d5f473202abb4301
Shafniamdt <iamshafnirafaideen@gmail.com>
2022-10-15 11:55:11 +0530

    Task3 commit

---
+++ Task3/Task3.js
@@ -0,0 +1,15 @@
+var car = {
+    model: "Benz",
+    color: "Black",
+    price: 60
+
+};
+
+if (car.price > 50){
+    console.log("expensive");
+}
+else{
+    console.log("affordable");
+}
+
+console.log(car.color);
