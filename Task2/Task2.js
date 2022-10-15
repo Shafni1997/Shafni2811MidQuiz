7fe53fae5298a9a5b2cb8cfd5dbbc9a6406c2c68
Shafniamdt <iamshafnirafaideen@gmail.com>
2022-10-15 11:47:31 +0530

    Task2 commit

---
+++ Task2/Task2.js
@@ -0,0 +1,22 @@
+var Movies = ["World War Z", "Lord of The Rings", "Harry Potter", "Tranformers", "Iron Man"];
+var FourthArrayVL = Movies[3];
+if (FourthArrayVL.charAt(0) === "I") {
+    console.log("Yay");
+}
+else {
+    console.log("Nay");
+}
+
+var Movies = [];
+Movies.push("World War Z")
+Movies.push("Lord of The Rings")
+Movies.push("Harry Potter")
+Movies.push("Iron Man")
+Movies.push("Tranformers")
+var FourthArrayVL = Movies[3];
+if (FourthArrayVL.charAt(0) === "I") {
+    console.log("Yay");
+}
+else {
+    console.log("Nay");
+}
